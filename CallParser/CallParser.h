afab67d032a41ab45004bb3f1cacdd9249cf0876
Peter Bourget <pbourget@w6op.com>
2020-06-06 08:43:13 -0700

    Initial Commit

---
+++ CallParser/CallParser.h
@@ -0,0 +1,19 @@
+//
+//  CallParser.h
+//  CallParser
+//
+//  Created by Peter Bourget on 6/6/20.
+//  Copyright © 2020 Peter Bourget. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CallParser.
+FOUNDATION_EXPORT double CallParserVersionNumber;
+
+//! Project version string for CallParser.
+FOUNDATION_EXPORT const unsigned char CallParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CallParser/PublicHeader.h>
+
+
